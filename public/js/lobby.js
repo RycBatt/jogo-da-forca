789055f14ea69870e7e0f06c02033b26a7dd9ce0
gabrielbdec <gabrielbdec@gmail.com>
2021-09-02 14:14:37 -0300

    Adição de borda caso usuário não tenha nick

--- public/js/lobby.js
+++ public/js/lobby.js
@@ -1,3 +1,5 @@
+/*jshint esversion: 6 */
+
 const lobby = {
   refreshList: function(list){
     let gList = document.getElementById("GamerList").querySelector("ul");
@@ -9,13 +11,14 @@ const lobby = {
       list.sort((a, b)=>{
         if (!a.ureOnIt && b.ureOnIt) return 1;
         else if (a.ureOnIt && !b.ureOnIt) return -1;
-        return 0
+        return 0;
       });
+      if (!list[0].ureOnIt) document.getElementById("nick").disabled = false;
       document.getElementById("GamerList").classList.remove("invisible");
       list.forEach((currentValue) => {
-        tipoBtn = currentValue.ureOnIt ? "Fechar" : "Jogar";
-        estiloBtn = currentValue.ureOnIt ? "btn-danger" : "btn-dark";
-        fncBtn = currentValue.ureOnIt ? `lobby.closeRoom("${currentValue.id}")` : `lobby.join("${currentValue.id}")`;
+        let tipoBtn = currentValue.ureOnIt ? "Fechar" : "Jogar";
+        let estiloBtn = currentValue.ureOnIt ? "btn-danger" : "btn-dark";
+        let fncBtn = currentValue.ureOnIt ? `lobby.closeRoom("${currentValue.id}")` : `lobby.join("${currentValue.id}")`;
         let liNew = document.createElement("li");
         liNew.innerHTML = (
           `<div class="row">
@@ -34,19 +37,52 @@ const lobby = {
     }
   },
   create(){
-    //Se faltar apelido, borda vermelha
-    connection.create(document.getElementById('nick').value);
-    document.getElementById("nick").disabled = true;
+    let nick = document.getElementById('nick').value;
+    if(nick == ""){
+      var nick_box = document.getElementById('nick');
+      nick_box.style.borderColor = 'red';
+      nick_box.style.boxShadow = '0px 0px 10px red';
+      nick_box.addEventListener("blur", (event)=>{
+        event.target.style.borderColor = "";
+        nick_box.style.boxShadow = '';
+      }, false);
+      nick_box.addEventListener("input", (event)=>{
+        event.target.style.borderColor = "blue";
+        nick_box.style.boxShadow = '0px 0px 10px blue';
+      }, false); 
+    } else {
+      connection.create(nick);
+      document.getElementById("nick").disabled = true;
+    }
   },
   closeRoom(id){
     connection.closeRoom(id);
   },
   join(id){
-    //Se faltar apelido, borda vermelha
-    connection.join(document.getElementById('nick').value, id);
+    let nick = document.getElementById('nick').value;
+    if(nick == ""){
+      var nick_box = document.getElementById('nick');
+      nick_box.style.borderColor = 'red';
+      nick_box.style.boxShadow = '0px 0px 10px red';
+      nick_box.addEventListener("blur", (event)=>{
+        event.target.style.borderColor = "";
+        nick_box.style.boxShadow = '';
+      }, false);
+      nick_box.addEventListener("input", (event)=>{
+        event.target.style.borderColor = "blue";
+        nick_box.style.boxShadow = '0px 0px 10px blue';
+      }, false); 
+    } else {
+      connection.join(nick, id);
+      document.getElementById("nick").disabled = true;
+    }
   }
-}
+};
 
-
-
-//document.getElementById("CreateRoom").addEventListener("click", ()=>{VOLTAR A BORDA AO NORMAL});
+window.onload = ()=>{
+  document.getElementById('nick').addEventListener("keyup", event => {
+    if(event.key !== "Enter") return;
+    document.getElementById('CreateRoom').click();
+    event.preventDefault();
+  });
+};
